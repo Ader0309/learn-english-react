8e4370a12e88c18a4bb2f1369c2f305f51a3ae84
Ader <boy2285256@gmail.com>
2024-07-10 11:13:21 +0800

    Important頁面初完成，待修

--- src/pages/Important.jsx
+++ src/pages/Important.jsx
@@ -10,6 +10,108 @@ const path = "http://localhost:3000";
 export default function Important() {
     const [fetching, setFetching] = useState(true);
     const [allEnglishList, setAllEnglishList] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [searchInput, setSearchInput] = useState("");
+    const [searchChinese, setSearchChinese] = useState("");
+    const [showSearch, setShowSearch] = useState(false);
+
+    const {
+        setPerPage,
+        setCurrentPage,
+        currentData,
+        currentPage,
+        perPage,
+        currentEnglishList,
+        pageNum,
+        handlePageClick,
+        handleNextPageClick,
+        handlePrevPageClick,
+    } = usePagination(allEnglishList);
+
+    function handleShowSearch() {
+        setShowSearch((prev) => !prev);
+    }
+
+    //點擊每頁幾筆
+    function handlePerPage(num) {
+        setPerPage(num);
+        setCurrentPage(1);
+    }
+
+    //依連線狀況 開啟modal
+    function responseStatus(response) {
+        if (response.status === "success") {
+            setModalTitle(response.message);
+            setShowModal(true);
+            setSearchChinese("");
+            getData();
+        } else if (response.status === "error") {
+            setModalTitle(response.message);
+            setShowModal(true);
+            setSearchChinese("");
+        }
+    }
+    // 加入/取消收藏 API
+    const addImportant = async (data) => {
+        const response = await fetch(`${path}/api/important`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res) => res.json());
+        try {
+            responseStatus(response);
+        } catch (err) {
+            return "取得資料失敗";
+        }
+    };
+
+    //查詢單字用
+    function handleSearchChange(e) {
+        setSearchInput(e.target.value);
+    }
+
+    // 查詢單字 API
+    const searchEnglish = async (data) => {
+        const response = await fetch(
+            `${path}/api/english?english=${searchInput}`
+        ).then((res) => res.json());
+        try {
+            if (response.status === "success") {
+                setModalTitle(response.message.english);
+                setSearchChinese(response.message.chinese);
+                setShowModal(true);
+            } else if (response.status === "error") {
+                setModalTitle(response.message);
+                setShowModal(true);
+                setSearchChinese("");
+            }
+        } catch (err) {
+            return "取得資料失敗";
+        }
+    };
+    function handleDoSearch() {
+        searchEnglish(searchInput);
+        setSearchInput("");
+    }
+
+    //刪除單字 API
+    const deleteEnglish = async (data) => {
+        const response = await fetch(`${path}/api/english`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res) => res.json());
+        try {
+            responseStatus(response);
+        } catch (err) {
+            return "取得資料失敗";
+        }
+    };
 
     //取所有英文單字 API
     const getData = async () => {
@@ -31,13 +133,42 @@ export default function Important() {
     useEffect(() => {
         getData();
     }, []);
+    useEffect(() => {
+        if (!fetching) {
+            currentData();
+        }
+    }, [allEnglishList, currentPage, perPage]);
     return (
         <>
-            <Modal />
-            <List fetching={fetching} currentEnglishList={allEnglishList}>
-                <ListTop />
+            <Modal
+                openModal={showModal}
+                closeModal={() => setShowModal(false)}
+                showCancelButton={false}
+                modalTitle={modalTitle}
+                searchChinese={searchChinese}
+            />
+            <List
+                fetching={fetching}
+                currentEnglishList={currentEnglishList}
+                addImportant={addImportant}
+                deleteEnglish={deleteEnglish}
+            >
+                <ListTop
+                    handleSearchChange={handleSearchChange}
+                    handleDoSearch={handleDoSearch}
+                    handlePerPage={handlePerPage}
+                    perPage={perPage}
+                    showSearch={showSearch}
+                    handleShowSearch={handleShowSearch}
+                />
             </List>
-            <PageList />
+            <PageList
+                pageNum={pageNum}
+                handlePageClick={handlePageClick}
+                currentPage={currentPage}
+                handleNextPageClick={handleNextPageClick}
+                handlePrevPageClick={handlePrevPageClick}
+            />
         </>
     );
 }
