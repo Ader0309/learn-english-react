f0b83e875c66c9765039bc327d0dd84594cddc15
Ader <boy2285256@gmail.com>
2024-07-21 20:51:22 +0800

    新增測試用帳密

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,8 @@ export default function Login() {
             <div className="login">
                 <MemberForm buttonTitle="登入" apiName="login" />
             </div>
+            <h5>測試用信箱:test@test.com</h5>
+            <h5>測試用暱稱:測試帳號1</h5>
         </>
     );
 }
